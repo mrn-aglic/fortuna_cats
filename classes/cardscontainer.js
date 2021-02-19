fdaa785a67dfca368b873187aebde2c2461be2f8
Marin Aglic Cuvic <>
2021-02-19 13:46:48 +0100

    Designed cards container and most of cards.

--- classes/cardscontainer.js
+++ classes/cardscontainer.js
@@ -17,27 +17,77 @@ export class CardsContainer {
         return img;
     }
 
+    createDetailsSection(cat) {
+
+        function createColorBox(color) {
+            const box = document.createElement('div');
+            box.classList.add('color-box');
+            box.style.backgroundColor = color;
+            return box;
+        }
+
+        function createSpan(text, cssClass) {
+            const span = document.createElement('span');
+            span.innerText = text;
+            span.classList.add(cssClass);
+            return span;
+        }
+
+        function createDetailRow(dat) {
+            const {text, data} = dat;
+
+            const div = document.createElement('div');
+            const label = createSpan(text, 'label');
+            const detail = createSpan(data, 'detail');
+
+            div.appendChild(label);
+            div.appendChild(detail);
+
+            div.classList.add('detail-row');
+
+            return div;
+        }
+
+        const details = document.createElement('div');
+
+        const monthsText = m => m === 1 ? 'mjesec' : m < 5 ? 'mjeseca' : 'mjeseci';
+
+        const {years, months} = cat.getAge();
+        const yearText = years > 0 ? `${years} godina` : '';
+        const monthText = months > 0 ? `${months} ${monthsText(months)}` : '';
+        const ageRow = createDetailRow({
+            text: 'Dob:',
+            data: [yearText, monthText].filter(t => t !== '').join(' i ')
+        });
+
+        const colorRow = createDetailRow({
+            text: 'Boja:',
+            data: cat.color
+        })
+        const colorBox = createColorBox(cat.color);
+        colorRow.lastChild.appendChild(colorBox);
+
+        const nameNode = document.createElement('h1');
+
+        nameNode.classList.add('detail');
+        nameNode.innerText = cat.name;
+
+        details.appendChild(nameNode);
+        details.appendChild(ageRow);
+        details.appendChild(colorRow);
+
+        details.classList.add('details-wrap');
+
+        return details;
+    }
+
     createCard(cat) {
         const img = this.createImgFromCat(cat);
 
         const container = document.createElement('div');
-        const details = document.createElement('div');
-
-        const nameNode = document.createElement('h1');
-        const ageNode = document.createElement('span');
-        const colorNode = document.createElement('span');
-
-        const {years, months} = cat.getAge();
-
-        nameNode.innerText = cat.name;
-        ageNode.innerText = `${years} godina i ${months} mjeseci`;
-        colorNode.innerText = cat.color;
+        const details = this.createDetailsSection(cat);
 
         container.classList.add('card');
-
-        details.appendChild(nameNode);
-        details.appendChild(ageNode);
-        details.appendChild(colorNode);
 
         container.appendChild(img);
         container.appendChild(details);
